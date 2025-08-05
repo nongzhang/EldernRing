393c50b5db44c40f4465cf61f50e820a45cbbc2f
nongzhang <965851140@qq.com>
2025-08-05 23:06:15 +0800

    随便改动，测试github配置完好

---
+++ Assets/Scripts/Character/AI Character/PursueTargetState.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NZ
+{
+    public class PursueTargetState : AIState
+    {
+        public override AIState Tick(AICharacterManager aICharacterManager)
+        {
+            return base.Tick(aICharacterManager);
+
+            //检查我们是否在执行一个动作(如果是，等动作执行完)
+            //检查
+        }
+    }
+}
